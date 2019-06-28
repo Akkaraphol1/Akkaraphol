2435d361e2682df6128a84cbb07f0e9c3e76f5e1
Akkaraphol1 <6206021630065@fitm.kmutnb.ac.th>
2019-06-28 17:45:00 +0700

    initial commit

---
+++ great.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	float b;
+    float c;
+   cout<<"Thai Baths : 250"<<endl;
+   cin>>b,c;
+   b = 250/30.7125307125;
+   c = 250/35.0631136045;
+   cout<<" 250 Baths is 8.14 dollars\n"<<b<<endl;
+   cin>>b;
+   cout<<" 250 Baths is 7.13 Euro\n"<<c<<endl;
+   return(0);
+}
